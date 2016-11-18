92578b06c7d1b0a3e5f083f39e9540f1e03df21d
Paul H Salmon <Pauly@MacBook-Pro.local>
2016-11-19 07:17:57 +1000

    responsive update

--- js/app.js
+++ js/app.js
@@ -10,8 +10,6 @@ $( document ).ready(function() {
     $('.social-media').toggleClass("hide-left");
   });
 
-
-
   $('.nav-ul>li').click(function(){
     console.log(this + ' was clicked')
     $('.menu-icon').removeClass("rotate")
@@ -22,7 +20,6 @@ $( document ).ready(function() {
     if(scrollPosition < 50){
       // $( ".hidden" ).fadeOut( "slow", "linear" );
 
-      $('.outer-home').removeClass( "opaque" );
       $('.navo').removeClass("hide-right")
       $('.menu-icon').removeClass("rotate")
       $('.social-media').removeClass("hide-left");
@@ -31,12 +28,31 @@ $( document ).ready(function() {
     } else {
       // $( ".hidden" ).fadeIn( "slow", "linear" );
       $('.menu-icon').removeClass("rotate")
-      $('.outer-home').addClass( "opaque" );
       $(".trigger").removeClass("hidden");
       $(".home").addClass("hide");
       $('.navo').addClass( "hide-right" );
       $('.social-media').addClass("hide-left");
     }
   })
+
+
+  $(document).scroll(function() {
+    var scrollPosition = $(document).scrollTop()
+    // console.log(scrollPosition)
+    if(scrollPosition > 310){
+      // $( ".main-image" ).fadeOut( "slow", function() {
+          $(".main-image img").addClass('opaque');
+      // });
+    };
+    if(scrollPosition < 310){
+      // $( ".main-image" ).fadeIn( "slow", function() {
+      $(".main-image img").removeClass('opaque');      // });
+    };
+    // if(scrollPosition < 256){
+    //   $( ".main-image" ).fadeIn( "slow", function() {
+    //       show();
+    //   });
+    // }
+  });
 
 });
